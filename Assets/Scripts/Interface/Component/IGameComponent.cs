aea2858de5bd733ea5a17c81769ba401f9816d7c
TanoVip123 <nguyentano200@gmail.com>
2026-05-19 23:54:10 -0700

    fix: Remove IGameComponent and IGameEntity

--- Assets/Scripts/Interface/Component/IGameComponent.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Game.Core.Interfaces
-{
-    // Marker interface for game components. Components are attached to entities to define data.
-    public interface IGameComponent { }
-}
--- Assets/Scripts/Interface/System/IGameSystem.cs
+++ Assets/Scripts/Interface/System/IGameSystem.cs
@@ -1,22 +1,19 @@
-﻿namespace Game.Core.Interfaces
+﻿// Marker interface for game systems. Systems contain logic that operates on entities with specific components.
+// Systems that implement this interface will be initialized and shut down by a Central Manager.
+public interface IGameSystem
 {
-    // Marker interface for game systems. Systems contain logic that operates on entities with specific components.
-    // Systems that implement this interface will be initialized and shut down by a Central Manager.
-    public interface IGameSystem
-    {
-        public void Initialize();
-        public void Shutdown();
-    }
+    public void Initialize();
+    public void Shutdown();
+}
 
-    // System that needs to be updated every frame should implement this interface. The Update method will be called with the time elapsed since the last frame.
-    public interface IUpdatableSystem
-    {
-        public void Update(float deltaTime);
-    }
+// System that needs to be updated every frame should implement this interface. The Update method will be called with the time elapsed since the last frame.
+public interface IUpdatableSystem
+{
+    public void Update(float deltaTime);
+}
 
-    // System that needs to be updated at fixed intervals (e.g., for physics updates) should implement this interface. The FixedUpdate method will be called with the fixed time step.
-    public interface IFixedUpdatableSystem
-    {
-        public void FixedUpdate(float fixedDeltaTime);
-    }
+// System that needs to be updated at fixed intervals (e.g., for physics updates) should implement this interface. The FixedUpdate method will be called with the fixed time step.
+public interface IFixedUpdatableSystem
+{
+    public void FixedUpdate(float fixedDeltaTime);
 }
